00f4021263645f5b01426da7d6dc05bdc61c3b4b
egandunning <dunninea@plu.edu>
2017-12-06 13:00:35 -0500

    added instructions in comments, fixed accidential deletion

--- day6.py
+++ day6.py
@@ -1,3 +1,37 @@
+#In this area, there are sixteen memory banks; each memory bank can hold any
+#number of blocks. The goal of the reallocation routine is to balance the
+#blocks between the memory banks.
+
+#The reallocation routine operates in cycles. In each cycle, it finds the
+#memory bank with the most blocks (ties won by the lowest-numbered memory
+#bank) and redistributes those blocks among the banks. To do this, it removes
+#all of the blocks from the selected bank, then moves to the next (by index)
+#memory bank and inserts one of the blocks. It continues doing this until it
+#runs out of blocks; if it reaches the last memory bank, it wraps around to
+#the first one.
+
+#The debugger would like to know how many redistributions can be done before
+#a blocks-in-banks configuration is produced that has been seen before.
+
+#Given the initial block counts in your puzzle input, how many
+#redistribution cycles must be completed before a configuration is produced
+#that has been seen before?
+
+import sys
+
+def getMaxBank(mem):
+   maxVal = 0
+   maxIndex = 0
+   for index in range(len(mem)):
+      if mem[index] > maxVal:
+         maxVal = mem[index]
+         maxIndex = index
+
+   return maxIndex, maxVal
+
+
+def main():
+
    if(len(sys.argv) != 2):
       print("Need cmd line arg for the file containing jump instructions.")
       return
@@ -14,18 +48,22 @@
 
    #loop until match is found
    match = False
-   count = 0
-   while not match:
+   cycle = 0
+   while True:
       pastBanks.append(mem[:])
       maxBank, maxBankVal = getMaxBank(mem)
       mem[maxBank] = 0
       for i in range(maxBank + 1, maxBankVal + maxBank + 1):
          mem[i % 16] += 1
 
-      count += 1
+      if match == True:
+         cycle += 1
       for pastBank in pastBanks[0:-1]:
          if mem == pastBank:
-            return count
+            if match == True:
+               return cycle + 1
+            match = True
+            pastBanks = pastBanks[-1:]
 
 
 
